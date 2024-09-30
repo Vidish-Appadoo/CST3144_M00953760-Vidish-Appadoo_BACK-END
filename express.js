6ba36edef72a0a2507b155850e59fb969295392e
Vidish Appadoo <krishnaappadoo99@gmail.com>
2024-09-30 21:44:06 +0400

    setting up server

--- express.js
+++ express.js
@@ -11,3 +11,10 @@ app.use(cors());
 
 // Connect to Mongo
 const client = new MongoClient(MONGO_URI);
+
+
+// Start the server
+const PORT =  3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
