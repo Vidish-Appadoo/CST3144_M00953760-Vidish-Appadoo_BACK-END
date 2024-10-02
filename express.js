9cc05af72b789385972847574ed1f489bba28683
Vidish Appadoo <krishnaappadoo99@gmail.com>
2024-10-02 11:01:41 +0400

    added PUT route api to update seats in mongoDB

--- express.js
+++ express.js
@@ -16,27 +16,54 @@ let db, lessonsCollection;
 
 // Connect to MongoDB
 async function connectToMongo() {
-  try {
+    try {
     await client.connect();
     db = client.db('CST3144_M00953760');  
     lessonsCollection = db.collection('LESSON');
     ordersCollection = db.collection('ORDERS');
     console.log('Connected to MongoDB');
-  } catch (error) {
+    } catch (error) {
     console.error('MongoDB connection failed:', error);
-  }
+    }
 }
 
 // API route to get lessons data and send it to front-end
 app.get('/lessons', async (req, res) => {
-  try {
+    try {
     const lessons = await lessonsCollection.find({}).toArray();
     console.log("Getting lessons data");
     res.json(lessons); // Send lessons data as JSON
     console.log("Sending lessons data to front-end");
-  } catch (error) {
+    } catch (error) {
     res.status(500).json({ message: 'Failed to fetch lessons data' });
-  }
+    }
+});
+
+// API route to update seats_available
+app.put('/lessons/update-seats', async (req, res) => {
+    const { lesson_title, location, action } = req.body;
+    try {
+        // Find the lesson in the database
+        const lesson = await lessonsCollection.findOne({ lesson_title, location });
+
+        //getting initial value of seats_available
+        let newSeatsAvailable = lesson.seats_available;
+
+        // Determine the new value for seats_available based on action
+        if (action === 'add') {
+        newSeatsAvailable--;
+        } else if (action === 'remove') {
+        newSeatsAvailable++;
+        }
+        // Update the seats_available field in the database
+        await lessonsCollection.updateOne(
+        { lesson_title, location },
+        { $set: { seats_available: newSeatsAvailable } }
+        );
+    } catch (error) {
+        console.error('Error updating seats:', error);
+        res.status(500).json({ message: 'Failed to update seats' });
+    }
 });
 
 connectToMongo();
@@ -44,7 +71,5 @@ connectToMongo();
 // Start the server
 const PORT =  3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
-//test
