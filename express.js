0a6e4ee7b5112cd502788aacfac5fe78c3968394
Vidish Appadoo <krishnaappadoo99@gmail.com>
2024-11-12 12:04:36 +0400

    trying to deploy on render.com

--- express.js
+++ express.js
@@ -105,7 +105,7 @@ app.get('/lessons/search', async (req, res) => {
 connectToMongo();
 
 // Start the server
-const port = process.env.PORT || 4000;
+const port = 10000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
